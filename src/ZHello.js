0f51ed84f055b054ea245df3f5a01e2427b27e5b
Matheus Kautzmann <kautzmann5@gmail.com>
2017-01-17 14:53:29 -0200

    Add react-native version of z-hello

---
+++ src/ZHello.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+  },
+  title: {
+    fontSize: 18,
+    fontFamily: 'Helvetica',
+    color: 'blue',
+  },
+});
+
+export function ZHello() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Hello!
+      </Text>
+    </View>
+  );
+}
+
+export default {
+  ZHello,
+};
